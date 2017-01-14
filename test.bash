f2ee32844690890cb4455e45ae7a80d18a5cf2ca
Hoon H <dev@eonil.com>
2017-01-14 20:47:18 +0900

    Make test script to generate Xcode project files at start.

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+swift package generate-xcodeproj
 xcodebuild -scheme EonilGCDActor -configuration Debug clean build test
 xcodebuild -scheme EonilGCDActor -configuration Release clean build
